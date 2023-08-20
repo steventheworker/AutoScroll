360a044e9df8e8ef78f59709500b4cedc7129b25
steventheworker <steventheworker@gmail.com>
2023-08-19 23:09:22 -0700

    new xcode project

---
+++ AutoScroll/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AutoScroll
+//
+//  Created by Steven G on 8/19/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
