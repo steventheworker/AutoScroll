e0e8e9bf9d5426c8669033f88e0bf6e223b180da
steventheworker <steventheworker@gmail.com>
2023-08-21 01:31:04 -0700

    fix mousemove (now works)

--- AutoScroll/src/helperLib.m
+++ AutoScroll/src/helperLib.m
@@ -329,7 +329,7 @@ void proc(CGDirectDisplayID display, CGDisplayChangeSummaryFlags flags, void* us
 + (void) listenScreens {CGDisplayRegisterReconfigurationCallback((CGDisplayReconfigurationCallBack) proc, (void*) nil);}
 + (void) listenMouseDown {[helperLib listenMask:CGEventMaskBit(kCGEventLeftMouseDown) | CGEventMaskBit(kCGEventRightMouseDown) | CGEventMaskBit(kCGEventOtherMouseDown) : (CGEventTapCallBack) handleMouseDown];}
 + (void) listenMouseUp {[helperLib listenMask:CGEventMaskBit(kCGEventLeftMouseUp) | CGEventMaskBit(kCGEventRightMouseUp) | CGEventMaskBit(kCGEventOtherMouseUp) : (CGEventTapCallBack) handleMouseUp];}
-+ (void) listenMouseMove {[helperLib listenMask:CGEventMaskBit(kCGEventMouseMoved) : (CGEventTapCallBack) handleMouseMove];}
++ (void) listenMouseMove {[helperLib listenMask:CGEventMaskBit(kCGEventMouseMoved) | CGEventMaskBit(kCGEventOtherMouseDragged) : (CGEventTapCallBack) handleMouseMove];}
 + (void) listenMask : (CGEventMask) emask : (CGEventTapCallBack) handler {
     CFMachPortRef myEventTap;
     CFRunLoopSourceRef eventTapRLSrc;
